7582ac57a87b337a3e406cbd3a8632f898937334
Fouad Chmainy <fchmainy@yahoo.com>
2023-06-14 18:50:58 +0200

    Update set_variables.sh

--- set_variables.sh
+++ set_variables.sh
@@ -12,7 +12,8 @@ export TF_VAR_namespace=$(echo $TF_VAR_owner | cut -d '@' -f1 | sed 's/\./-/')
 #echo "importing SSH Key"
 #rm -f ~/.ssh/id_rsa*
 #ssh-keygen -b 2048 -t rsa -f /home/ubuntu/.ssh/id_rsa -q -N ""
-export TF_VAR_ssh_key=$(cat /home/ubuntu/.ssh/id_rsa.pub)
+#export TF_VAR_ssh_key=$(cat /home/ubuntu/.ssh/id_rsa.pub)
+export TF_VAR_ssh_key="null"
 echo $TF_VAR_ssh_key
 
 export TF_VAR_trusted_ip=$(curl -s ifconfig.me)
